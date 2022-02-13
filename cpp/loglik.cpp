665d559a782fd2b3789de61d350df1a7c7cb60a9
vkyo23 <99581345+vkyo23@users.noreply.github.com>
2022-02-14 00:24:14 +0900

    Update loglik.cpp

--- cpp/loglik.cpp
+++ cpp/loglik.cpp
@@ -6,10 +6,20 @@ using namespace Rcpp;
 // Log-likelihood function
 arma::mat loglik(arma::mat Y, arma::vec alpha, 
                  arma::vec beta, arma::vec theta) {
-  arma::mat temp = beta * theta.t();
-  arma::mat temp2 = temp.each_col() - alpha;
-  arma::mat exp_ = arma::exp(temp2.t());
+  //calculate beta_j * theta_i
+  arma::mat temp = beta * theta.t(); 
+  
+  //beta_j * theta_i - alpha_j
+  arma::mat temp2 = temp.each_col() - alpha; 
+  
+  //exp(beta_j * theta_i - alpha_j)
+  arma::mat exp_ = arma::exp(temp2.t()); 
+  
+  //inverse logit
   arma::mat p = exp_ / (1 + exp_);
+  
+  //calculate log-lik
   arma::mat log_lik = Y % arma::log(p) + (1 - Y) % arma::log(1 - p);
+  
   return(log_lik);
 }
